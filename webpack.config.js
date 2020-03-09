96eb351c1002ba95a47438dd08e55fa2bf79a8dc
Sergey Klimenko <klimenko.sg@gmail.com>
2020-03-09 19:09:07 +0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,100 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 if (process.env.NODE_ENV === 'test') {
-    require('dotenv').config({ path: '.env.test'});
+  require('dotenv').config({ path: '.env.test' });
 } else {
-    require('dotenv').config({ path: '.env.development' });
+  require('dotenv').config({ path: '.env.development' });
 }
 
-module.exports = (env) => {
-    const isProduction = (env === 'production');
-    return {
-        mode: isProduction ? 'production' : 'development',
-        entry: './src/app.js',
-        output : {
-            path: path.join(__dirname, 'public', 'dist'),
-            filename: 'bundle.js'
+module.exports = env => {
+  const isProduction = env === 'production';
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].[contenthash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader'
+            }
+          ]
         },
-        module: {
-            rules: [{
-                test: /.js$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader'
-                }]
-            }, {
-                test: /.s?css$/,
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        hmr: !isProduction
-                    }
-                }, {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'sass-loader'
-                }]
-            }]
+        {
+          test: /.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: !isProduction
+              }
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
         },
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            publicPath: '/dist/',
-            port: 8080,
-            historyApiFallback: true,
-            open: true
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: 'styles.css'
-            }),
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-                'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
-            })
-        ]
-    }
-}
+        {
+          test: /\.gif$|\.png$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img'
+          }
+        }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'build'),
+      port: 8080,
+      historyApiFallback: true,
+      open: true
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
+        ),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        )
+      }),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        minify: true
+      })
+    ]
+  };
+};
